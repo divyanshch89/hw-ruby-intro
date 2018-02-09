b193516e11c2ffda20c9764fb9e727a5404f9844
EC2 Default User <ec2-user@ip-172-31-22-110.us-east-2.compute.internal>
2018-02-09 21:42:50 +0000

    Added solution for part 2 and 3

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -50,21 +50,62 @@ def hello(name)
   "Hello, "+ name
 end
 
-def starts_with_consonant? s
+def starts_with_consonant?( s)
   # YOUR CODE HERE
+  if s.empty? || !valid?(s)
+    return false
+  end
+  vowel=['a','e','i','o','u']
+  !vowel.include? s.downcase[0]
 end
 
-def binary_multiple_of_4?( s)
+def valid?(str)
+  (str =~ /[^a-zA-Z0-9]/).nil?
+end
+
+def binary_multiple_of_4?(s)
   # YOUR CODE HERE
-  if s.to_i(2)>0
+  if !s.empty? && validbinary?(s)
   s.to_i(2)%4==0
   else
     false
   end
 end
 
+def validbinary?(s)
+ s !~ /[^01]/
+end
+
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  
+  def initialize(isbn,price)
+    if isbn.empty?|| price<=0
+      raise ArgumentError
+    end
+    
+    @isbn,@price=isbn,price
+  end
+  
+  def price
+    @price
+  end
+  
+  def price=(value)
+    @price=value
+  end
+  
+  def isbn
+    @isbn
+  end
+  
+  def isbn=(value)
+    @isbn=value
+  end
+  
+  def price_as_string()
+    "$".concat("%.2f" % @price)
+  end
+    
 end
